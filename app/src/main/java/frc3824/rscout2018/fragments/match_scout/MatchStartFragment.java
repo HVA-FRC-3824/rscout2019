068ff6079dac55c3f9bcae19847a90e512a4973b
Knox Crichton <knoxcrichton@gmail.com>
2019-01-30 21:00:11 -0500

    Continued work to implement pop up changes, more comments, builds successfully no crashes currently

--- app/src/main/java/frc3824/rscout2018/fragments/match_scout/MatchStartFragment.java
+++ app/src/main/java/frc3824/rscout2018/fragments/match_scout/MatchStartFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -19,12 +20,14 @@ import frc3824.rscout2018.database.data_models.TeamMatchData;
 import frc3824.rscout2018.database.data_models.TeamPitData;
 import frc3824.rscout2018.databinding.FragmentMatchStartBinding;
 import frc3824.rscout2018.utilities.Utilities;
+import frc3824.rscout2018.views.powered_up.SavableCubes;
 
 /**
  * @author frc3824
  */
 public class MatchStartFragment extends MatchScoutFragment implements ImageListener
 {
+    SavableCubes mCubes = null;
     private FragmentMatchStartBinding mBinding = null;
     private CarouselView mCarouselView;
     private ArrayList<String> mPictureFilepaths;
@@ -79,4 +82,31 @@ public class MatchStartFragment extends MatchScoutFragment implements ImageListe
         }
         // todo(Andrew): error
     }
+
+////  Testing java implementation of cubes pop up
+//    /**
+//     * {@inheritDoc}
+//     */
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+//    {
+//        // Inflate layout and bind the data model
+//        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_match_auto, container, false);
+//        bind();
+//        View view = mBinding.getRoot();
+//
+//        // Inflate the cubes view
+//        mCubes = view.findViewById(R.id.cubes);
+//        mCubes.setAuto(true);
+//
+//        // Inflate the undo button and pass it to the cubes view
+//        Button undo = view.findViewById(R.id.undo);
+//        undo.setVisibility(View.GONE);
+//        mCubes.setUndoButton(undo);
+//
+//        // Add touch listeners
+//        Utilities.setupUi(getActivity(), view);
+//
+//        return view;
+//    }
 }
